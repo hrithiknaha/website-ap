c7e2eca7c0e862d65fcc28ded2a3f6f1ccbdf8e2
neil <naha.hrithkk@gmail.com>
2020-08-16 17:02:44 +0530

    Home page commit

---
+++ components/HomeBody.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const HomeBody = () => {
+	return (
+		<div id='about' className='homebody'>
+			<p>
+				When I m not outside with my rucksack, I love working for
+				WordPress. I find it a great platform to express my thoughts.
+			</p>
+			<p>
+				I live in Kolkata, with my family. My favorite things in life
+				are a big plate of paella with a glass of sangria , windy days
+				and going out to the dog park.
+			</p>
+
+			<blockquote>
+				The cure of boredom is curiosity. There is no cure for
+				curiosity.
+				<cite>~ Aditi Pandey</cite>
+			</blockquote>
+
+			<p>
+				Hi! I am Aditi Pandey. I am an Electronics Engineer and an
+				eloquent orator. I am a soulful fashion enthusiast who is a
+				caffeine connoisseur and loves travelling. I watch horror movies
+				and get scared by lizards. But, I wish them good luck!
+			</p>
+
+			<a href='/blogs'>
+				<button class='homebody-button ui green button'>
+					Read my blogs
+				</button>
+			</a>
+		</div>
+	);
+};
+
+export default HomeBody;
--- components/HomePage.js
+++ components/HomePage.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
+import HomeBody from './HomeBody';
+
 const HomePage = () => {
 	return (
 		<div>
 			<div className='homepage-image'>
+				<div className='homepage-image_nav'>
+					<a href='/'>
+						<p>HOME</p>
+					</a>
+					<a href='/blogs'>
+						<p>BLOGS</p>
+					</a>
+					<a href='#about'>
+						<p>ABOUT ME</p>
+					</a>
+				</div>
 				<img
 					src='https://source.unsplash.com/random/1600x900'
 					alt='Home image background image'
 				/>
+				<p>ADITI PANDEY</p>
 			</div>
+			<HomeBody />
 		</div>
 	);
 };
