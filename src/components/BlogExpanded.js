8e1376386d1524d32abc520536545a0412f04fdc
neil <naha.hrithkk@gmail.com>
2020-08-27 15:29:17 +0530

    Add blog style

--- src/components/BlogExpanded.js
+++ src/components/BlogExpanded.js
@@ -3,9 +3,24 @@ import parse from 'html-react-parser';
 
 const BlogExpanded = (props) => {
 	return (
-		<div>
-			<h1>{props.location.state.blog.title}</h1>
-			<p>{parse(props.location.state.blog.body)}</p>
+		<div className='wrapper'>
+			<div className='wrapper-blog'>
+				<div className='wrapper-blog-head'>
+					<img
+						className='wrapper-blog-head_img'
+						src='https://source.unsplash.com/random/400x400'
+						alt=''
+					/>
+
+					<div className='wrapper-blog-head_title'>
+						{props.location.state.blog.title}
+					</div>
+				</div>
+
+				<div className='wrapper-blog_body'>
+					{parse(props.location.state.blog.body)}
+				</div>
+			</div>
 		</div>
 	);
 };
